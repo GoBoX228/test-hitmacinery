d9a736d82e6513ce6cf8a4cd21cd6a966df3abca
GoBoX228 <vilyougiv@gmail.com>
2024-10-29 19:26:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite';
-import vitePugPlugin from 'vite-plugin-pug-transformer';
-
 export default defineConfig({
+  base: '',
+  outDir: 'dist',
+  assetsDir: 'assets',
   plugins: [vitePugPlugin()],
-    server: {
-      watch: {
-        ignored: [   
-          '!src/components/**/*.pug' 
-        ]
-      }
-    },
-    build: {
-      rollupOptions: {
-        input: {
-          main: './index.html',
-          second: './second.html'
-        }
+  server: {
+    watch: {
+      ignored: [
+        '!src/components/**/*.pug'
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        second: './second.html'
       }
     }
-  });
-  
+  }
+});
